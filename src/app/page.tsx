f769dcdce01546ea71ed3cef435d4bda80b59bd5
yusupsupriyadi <yusupsupriyadistudio@gmail.com>
2025-02-20 19:46:07 +0700

    feat(main feature )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,32 @@
 'use client';
-import SearchBar from './components/SearchBar';
-import UserProfile from './components/UserProfile';
-import RepoList from './components/RepoList';
-import ReadmeViewer from './components/ReadmeViewer';
-import { useGithubStore } from './store/githubStore';
-import './styles/style.css';
+import SearchBar from './../components/SearchBar';
+import './../styles/style.css';
+import UserProfile from '@/components/UserProfile';
+import RepoList from '@/components/RepoList';
+import ReadmeViewer from '@/components/ReadmeViewer';
+import { useGithubStore } from '@/store/githubStore';
 
 export default function Home() {
 	const { user, selectedRepo, isLoading, error } = useGithubStore();
 
 	return (
-		<div className="page">
-			<main className="main">
-				<h1 className="title">
-					GitHub Repository Explorer
-				</h1>
+		<div className='page'>
+			<main className='main'>
+				<h1 className='title'>GitHub Repository Explorer</h1>
 				<SearchBar />
 				{isLoading && (
-					<div className="loading-text">
+					<div className='loading-text'>
 						<p>Sedang memuat...</p>
 					</div>
 				)}
 				{error && (
-					<div className="error-text">
+					<div className='error-text'>
 						<p>{error}</p>
 					</div>
 				)}
 				{user && (
-					<div className="content">
-						<div className="left-column">
+					<div className='content'>
+						<div className='left-column'>
 							<UserProfile />
 							<RepoList />
 						</div>
